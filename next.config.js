0cdafd720a5174ef2cff116bdbdeb42ee99d455c
Shreeram <deepskinweb@gmail.com>
2023-04-20 23:54:39 +0530

    April v1 21 2023

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
- 
+  compress: true,
+  images: {
+    formats: ['image/webp']
+}
 }
 
 module.exports = nextConfig
