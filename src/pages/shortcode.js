176f1fe29018054eb45eb7510e2ede351982b506
Shreeram <deepskinweb@gmail.com>
2024-01-09 23:14:19 +0530

    Date 09 Jan 2023

---
+++ src/pages/shortcode.js
@@ -0,0 +1,21 @@
+// pages/[shortcode].js
+import { useRouter } from 'next/router';
+
+const ShortcodePage = () => {
+  const router = useRouter();
+  const { shortcode } = router.query;
+
+  // Perform logic with the shortcode, e.g., redirect to app.linck.one
+  if (shortcode) {
+    // Redirect logic or any other processing
+    console.log(`Redirecting to https://app.linck.one/${shortcode}`);
+  }
+
+  return (
+    <div>
+      <p>Content for shortcode: {shortcode}</p>
+    </div>
+  );
+};
+
+export default ShortcodePage;
